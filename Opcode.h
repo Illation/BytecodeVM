887105664b435b14e51dc0cc21c59df9ce05e55f
Robert <contact@robert-lindner.com>
2017-09-03 15:52:49 +0200

    removed hardcoded hello world program from opcode

--- Opcode.h
+++ Opcode.h
@@ -13,35 +13,3 @@ static std::map<std::string, Opcode> OpcodeNames
     {"LITERAL", Opcode::LITERAL},
     {"PRINT", Opcode::PRINT}
 };
-
-static int TestBytecodeSize = 27;
-static char TestBytecode[] = 
-{
-    (char)Opcode::LITERAL,  
-    'H', 
-    (char)Opcode::LITERAL,  
-    'e', 
-    (char)Opcode::LITERAL,  
-    'l', 
-    (char)Opcode::LITERAL,  
-    'l', 
-    (char)Opcode::LITERAL,  
-    'o', 
-    (char)Opcode::LITERAL,  
-    ' ', 
-    (char)Opcode::LITERAL,  
-    'W', 
-    (char)Opcode::LITERAL,  
-    'o', 
-    (char)Opcode::LITERAL,  
-    'r', 
-    (char)Opcode::LITERAL,  
-    'l', 
-    (char)Opcode::LITERAL,  
-    'd', 
-    (char)Opcode::LITERAL,  
-    '!', 
-    (char)Opcode::LITERAL,  
-    static_cast<char>(12), 
-    (char)Opcode::PRINT
-};
